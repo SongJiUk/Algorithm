b641b0befd06a373eab62443daf638775b11fed1
SongJiUk <67099323+SongJiUk@users.noreply.github.com>
2024-02-27 17:21:48 +0900

    Update 인기가 넘쳐흘러.cc

--- 백준/Platinum/17258. 인기가 넘쳐흘러/인기가 넘쳐흘러.cc
+++ 백준/Platinum/17258. 인기가 넘쳐흘러/인기가 넘쳐흘러.cc
@@ -72,3 +72,74 @@ int main(void)
     cout << DFS(0, K, 0) << endl;
 
 }
+
+
+
+//이건 시간 초과된 문제 (맞긴맞는데 시간초과 뜸)
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+#define MAX 1e9
+
+using namespace std;
+
+
+vector<pair<int,int>> vec;
+vector<int> now(302, 0);
+int DP[302][302];
+int N, M, K, T;
+
+int DFS(int _time, int _remain_f, int _prev_f)
+{
+    if(_time > N) return 0;
+
+    int &ret = DP[_time][_remain_f];
+
+    if(now[_time] >= T) //초대받은 사람이 T명 이상일경우(선영이 친구들이 들어갈 이유가 없음)
+    {   
+        ret = DFS(_time + 1, _remain_f, 0) + 1; //1을 더해주는 이유는 값을 1씩 올리기 위해.
+    }
+    else // 초대받은 사람이 T명 미만일경우
+    {
+        if(now[_time] + _prev_f >= T) //이미 투입되어 있는 친구껴서 T명 이상일경우
+        {
+            ret = DFS(_time+1, _remain_f, _prev_f) + 1;
+        }
+        else // 투입되어 있는 애들 껴도 T미만일경우
+        {
+            if(now[_time] + _prev_f + _remain_f >= T) //남은 친구를 껴서 T 이상인 경우
+            {
+                int tmp = T - (now[_time] + _prev_f); // 몇명이 부족한지 확인(_remain_f 관리)
+
+                ret = max(DFS(_time + 1, _remain_f - tmp, _prev_f + tmp) +1, DFS(_time+1, _remain_f, _prev_f)); //집어 넣는경우와, 그렇지 않은경우 확인.
+
+            }
+            else //그래도 안되는 경우
+            {   
+                ret = DFS(_time+1, _remain_f, _prev_f);
+            }
+        }
+    }
+
+    return ret;
+}
+
+int main(void)
+{
+    
+    cin >> N >> M >> K >> T;
+
+    for(int i =0; i<M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        for(int j = a; j < b; j++)
+        {
+            now[j] = min(T, ++now[j]);
+        }
+    }
+    cout << DFS(1, K, 0) << endl;
+
+}
