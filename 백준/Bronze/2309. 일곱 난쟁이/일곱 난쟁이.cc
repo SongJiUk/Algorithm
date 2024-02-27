892a5a60e161dea6f643522f3bea6f316a5d3191
SongJiUk <67099323+SongJiUk@users.noreply.github.com>
2024-02-27 17:13:05 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+#define endl '\n'
+#define MAX 9
+using namespace std;
+
+
+
+vector<int> vec;
+
+vector<int> v;
+
+bool isVisit[MAX];
+bool flag = false;
+void DFS(int _count, int _sum)
+{
+    if(_count == 7) 
+    {
+        if(_sum == 100) 
+        {
+            if(!flag)
+            {
+                flag = true;
+                for(int i =0; i< MAX; i++)
+                {
+                    if(isVisit[i]) v.push_back(vec[i]);
+                }
+            }
+            
+            return;
+        }
+    }
+    else
+    {
+        for(int i =0; i<MAX; i++)
+        {
+            if(isVisit[i]) continue;
+            
+            _sum += vec[i];
+            isVisit[i] = true;
+            _count++;
+
+            DFS(_count, _sum);
+            
+            _sum -= vec[i];
+            isVisit[i] = false;
+            _count--;
+            
+        }
+    }
+}
+
+
+int main(void)
+{
+    for(int i =0; i<MAX; i++)
+    {
+        int x;
+        cin >> x;
+        vec.push_back(x);
+    }
+
+
+    DFS(0, 0);
+
+    sort(v.begin(), v.end());
+
+
+    for(int i =0; i<v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+}
