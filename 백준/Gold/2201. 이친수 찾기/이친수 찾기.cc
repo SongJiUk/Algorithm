be5db564c3a11bd4f3daaa322814362ddd0ca203
SongJiUk <67099323+SongJiUk@users.noreply.github.com>
2024-03-05 17:50:40 +0900

    Update 이친수 찾기.cc

--- 백준/Gold/2201. 이친수 찾기/이친수 찾기.cc
+++ 백준/Gold/2201. 이친수 찾기/이친수 찾기.cc
@@ -11,6 +11,7 @@
 using namespace std;
 
 long long DP[90];
+//1번 방법
 int main(void)
 {
     ios::sync_with_stdio(false);
@@ -42,6 +43,50 @@ int main(void)
             }
             else cout << "0";
         }
+    }
+    cout << endl;
+}   
+
+//2번방법
+
+long long DP[90];
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long K;
+    cin >> K;
+
+    //자리수의 시작 지점을 저장.
+    DP[1] = 1;
+    DP[2] = 2;
+
+    for(int i =3; i<=90; i++)
+    {
+        DP[i] = DP[i-1] + DP[i-2];
+    }
+
+    int length = 0;
+    for(int i = 1; i<=90; i++)
+    {
+        if(DP[i] > K)
+        {
+            length = i - 1;
+            break;
+        }
+    }
+
+    while(length >0)
+    {
+        if(DP[length] <= K)
+        {
+            cout << 1;
+            K -= DP[length];
+        }
+        else cout << 0;
+
+        length--;
     }
     cout << endl;
 }   
